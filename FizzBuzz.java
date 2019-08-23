9520ad83f7e020f062bf600829b2a447f38a3d42
Austin Parham <austparh@amazon.com>
2019-08-23 12:25:52 -0700

    Conditionals

---
+++ FizzBuzz.java
@@ -0,0 +1,11 @@
+public class FizzBuzz{
+	public static void main(String[] args){
+		int number = 5;
+		if(number % 3 == 0){
+			System.out.println("Fizz");
+		}
+		else if(number % 5 == 0){
+			System.out.println("Buzz");
+		}
+	}
+}
