f7ae94b3efb062db78f9fc5b436b813a8639a47a
Austin Parham <austparh@amazon.com>
2020-01-31 10:27:08 -0600

    Exceptions practice

---
+++ Exceptions.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class Exceptions{
+    public static void main(String[] args){
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        System.out.println(myList);
+
+        for(int i = 0; i < myList.size(); i++) {
+            try{
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println(castedValue);
+            }
+            catch (Exception e){
+                System.out.println("Something went wrong...");
+                System.out.println(myList.get(i));
+            }
+        }
+    }
+}
